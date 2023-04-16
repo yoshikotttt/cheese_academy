096d07e0a35ca914c429fb749552f73beef52fa9
yoshikotttt <myqic.n@gmail.com>
2023-04-16 15:41:39 +0900

    js追加

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 // jsを記述する際はここに記載していく
+  const pagetop_btn = document.querySelector(".pagetop");
+
+
+pagetop_btn.addEventListener("click", scroll_top);
+
+
+function scroll_top() {
+  window.scroll({ top: 0, behavior: "smooth" });
+}
+
+window.addEventListener("scroll", scroll_event);
+function scroll_event() {
+  if (window.pageYOffset > 100) {
+    pagetop_btn.style.opacity = "1";
+  } else if (window.pageYOffset < 100) {
+    pagetop_btn.style.opacity = "0";
+  }
+}
+
